380b70125b70774bfd2f7c4b042b50db890e406d
Aviral <chauhanaviral79@gmail.com>
2023-12-13 14:02:41 +0530

    added toggle button functionality

---
+++ button.js
@@ -0,0 +1,25 @@
+var toggleButton = document.getElementById("switch");
+var basiccontent = document.getElementById("basicPricing");
+var professionalcontent = document.getElementById("professionalPricing");
+var mastercontent = document.getElementById("masterPricing");
+toggleButton.addEventListener("change", function () {
+  if (toggleButton.checked) {
+    basiccontent.innerHTML = "&dollar;19.99";
+  } else {
+    basiccontent.innerHTML = "&dollar;199.99";
+  }
+});
+toggleButton.addEventListener("change", function () {
+  if (toggleButton.checked) {
+    professionalcontent.innerHTML = "&dollar;24.99";
+  } else {
+    professionalcontent.innerHTML = "&dollar;249.99";
+  }
+});
+toggleButton.addEventListener("change", function () {
+  if (toggleButton.checked) {
+    mastercontent.innerHTML = "&dollar;39.99";
+  } else {
+    mastercontent.innerHTML = "&dollar;399.99";
+  }
+});
